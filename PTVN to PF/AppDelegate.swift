d70af123f9c2de4c6a7fdd3623001de0bd24787d
FoolOfShadows <foolofshadows@gmail.com>
2015-12-09 09:03:20 -0600

    Initial Commit

---
+++ PTVN to PF/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PTVN to PF
+//
+//  Created by Fool on 12/9/15.
+//  Copyright © 2015 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ PTVN to PFTests/PTVN_to_PFTests.swift
@@ -0,0 +1,36 @@
+//
+//  PTVN_to_PFTests.swift
+//  PTVN to PFTests
+//
+//  Created by Fool on 12/9/15.
+//  Copyright © 2015 Fulgent Wake. All rights reserved.
+//
+
+import XCTest
+@testable import PTVN_to_PF
+
+class PTVN_to_PFTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
